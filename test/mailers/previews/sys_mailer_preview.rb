d21f228eb461045acae773fb8f7b93dbbe8f9e6d
Andrew Liu <andcliu@Andrews-Air-2.hsd1.ca.comcast.net>
2024-11-01 19:26:29 -0700

    udpate

--- test/mailers/previews/sys_mailer_preview.rb
+++
@@ -1,9 +0,0 @@
-# Preview all emails at http://localhost:3000/rails/mailers/sys_mailer
-class SysMailerPreview < ActionMailer::Preview
-
-  # Preview this email at http://localhost:3000/rails/mailers/sys_mailer/waiver_email
-  def waiver_email
-    SysMailer.waiver_email
-  end
-
-end
--- test/models/afns_class_schedule_test.rb
+++
@@ -1,8 +0,0 @@
-require 'test_helper'
-
-class AfnsClassScheduleTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
-
